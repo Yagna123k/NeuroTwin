fe4c51b17b307cf29d1c55630f64a584dfa90c1e
Yagna Kusumanchi <yagna82kb@gmail.com>
2025-06-07 11:29:19 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import About from './components/About';
 import UseCases from './components/UseCases';
@@ -10,7 +9,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-[#0D1117]">
-      <Navigation />
       <Hero />
       <About />
       <UseCases />
