fe57e7bc7af7d9a9d63a03e25588070859aa3c60
Yagna Kusumanchi <yagna82kb@gmail.com>
2025-06-07 17:48:18 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About'
 import AboutPage from './components/AboutPage';
 import UseCases from './components/UseCases';
 import PersonaDemo from './components/PersonaDemo';
